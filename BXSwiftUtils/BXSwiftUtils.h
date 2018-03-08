114b57311b80593aa2a35ab6d82e985f37dbad16
Stefan Fochler <istefo@me.com>
2018-03-08 22:50:12 +0100

    Initialize Xcode project

---
+++ BXSwiftUtils/BXSwiftUtils.h
@@ -0,0 +1,19 @@
+//
+//  BXSwiftUtils.h
+//  BXSwiftUtils
+//
+//  Created by Stefan Fochler on 08.03.18.
+//  Copyright © 2018 Boinx Software Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BXSwiftUtils.
+FOUNDATION_EXPORT double BXSwiftUtilsVersionNumber;
+
+//! Project version string for BXSwiftUtils.
+FOUNDATION_EXPORT const unsigned char BXSwiftUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BXSwiftUtils/PublicHeader.h>
+
+
