3736777ca717713319df5c481a8fff558bf8294f
Stefan Fochler <istefo@me.com>
2018-03-09 15:53:16 +0100

    Document & simplify BXLoggerTests

--- BXSwiftUtilsTests/Logging/BXLoggerTests.swift
+++ BXSwiftUtilsTests/Logging/BXLoggerTests.swift
@@ -11,87 +11,102 @@ import BXSwiftUtils
 
 class BXLoggerTests: XCTestCase {
 
+    /*
+     Asserts that two log levels are comparable.
+     */
     func testLevelComparable()
     {
         XCTAssertGreaterThan(BXLogger.Level.verbose, BXLogger.Level.warning)
     }
 
+    /*
+     Asserts that a single message gets logged to a destination added via `addDestination`.
+     */
     func testAddDestination()
     {
-        let expect = expectation(description: "Destination Called")
+        var count = 0
         
         var logger = BXLogger()
-        logger.addDestination { (level, message) in
-           expect.fulfill()
+        
+        logger.addDestination
+        { (level, message) in
+           count += 1
         }
+        
         logger.print(level: .error, force: false, showLocation: false, message: { "bla" })
         
-        waitForExpectations(timeout: 0.1, handler: nil)
+        XCTAssertEqual(count, 1, "printed once")
     }
     
+    /*
+     Asserts that the default max log level swallows verbose and debug messages but not warnings and errors.
+     This is useful to make sure that the default max log level is not simply changed at some point in the future and
+     thus unexpectedly effects the consumers of the BXLogger class.
+     */
     func testDefaultLogLevel()
     {
-        let expect = expectation(description: "Destination Called Twice")
-        
-        var count = 0;
+        var count = 0
         
         var logger = BXLogger()
         
-        logger.addDestination { (level, message) in
+        logger.addDestination
+        { (level, message) in
             count += 1
             
             XCTAssert(level == .warning || level == .error, "level is warning or error")
-            
-            if count == 2
-            {
-                expect.fulfill()
-            }
         }
+        
         logger.verbose { "verbose" }
         logger.debug(showLocation: true) { "debug" }
         logger.warning { "warning" }
         logger.error { "error" }
         
-        waitForExpectations(timeout: 0.1, handler: nil)
+        XCTAssertEqual(count, 2, "printed two times")
     }
     
+    /*
+     Asserts that settings a custom log level changes the default behavior regarind swallowing/forwarding events.
+     */
     func testCustomLogLevel()
     {
-        let expect = expectation(description: "Destination Called Twice")
-        
-        var count = 0;
+        var count = 0
         
         var logger = BXLogger()
+        
         logger.maxLevel = .debug
-        logger.addDestination { (level, message) in
+        
+        logger.addDestination
+        { (level, message) in
             count += 1
             
             XCTAssert(level == .debug || level == .warning || level == .error, "level is debug, warning, or error")
-            
-            if count == 3
-            {
-                expect.fulfill()
-            }
         }
+        
         logger.verbose { "verbose" }
         logger.debug { "debug" }
         logger.warning { "warning" }
         logger.error(showLocation: true) { "error" }
         
-        waitForExpectations(timeout: 0.1, handler: nil)
+        XCTAssertEqual(count, 3, "printed three times")
     }
     
+    /*
+     Asserts that messages can be forced through the log level filter if desired.
+     */
     func testForceLog()
     {
-        let expect = expectation(description: "Destination Called")
+        var count = 0
         
         var logger = BXLogger()
-        logger.addDestination { (level, message) in
+        
+        logger.addDestination
+        { (level, message) in
             // log shouldn't pass the default max log level, but is forced
-            expect.fulfill()
+            count = 1
         }
+        
         logger.print(level: .verbose, force: true, showLocation: false, message: { "bla" })
         
-        waitForExpectations(timeout: 0.1, handler: nil)
+        XCTAssertEqual(count, 1, "printed once")
     }
 }
