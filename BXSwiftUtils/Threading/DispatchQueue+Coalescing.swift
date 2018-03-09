811ee4cba6c02f7bfd0b02796761e95c78ac7a12
Peter Baumgartner <peter@boinx.com>
2018-03-09 11:42:18 +0100

    Applied style guide

--- BXSwiftUtils/Threading/DispatchQueue+Coalescing.swift
+++ BXSwiftUtils/Threading/DispatchQueue+Coalescing.swift
@@ -15,13 +15,18 @@ import Foundation
 
 public extension DispatchQueue
 {
-	/// asyncCoalesced() is similar to async(), but offers additional funtionality: If multiple blocks are submitted
-	/// within a given time internal, only the last one will be executed. Please note that this method should only
-	/// be called from the main thread.
-	/// - parameter identifier: The identifier string that is used to coalesce multiple blocks together
-	/// - parameter delay: The time interval (in seconds)
-	/// - parameter block: The worker block (closure) to be submitted
-
+	/**
+	
+	asyncCoalesced() is similar to async(), but offers additional funtionality: If multiple blocks are submitted
+	within a given time internal, only the last one will be executed. Please note that this method should only
+	be called from the main thread.
+	
+	- parameter identifier: The identifier string that is used to coalesce multiple blocks together
+	- parameter delay: The time interval (in seconds)
+	- parameter block: The worker block (closure) to be submitted
+	
+	*/
+	
 	public func asyncCoalesced( identifier: String, delay: Double=0.0, block: @escaping ()->Void)
 	{
 		if Thread.isMainThread
@@ -41,16 +46,15 @@ public extension DispatchQueue
 		else
 		{
 			let stacktrace = Thread.callStackSymbols.joined(separator:"\n")
-            // TODO: This line currently doesn't compile because log can't be found
-			//log.error { "ERROR DispatchQueue.\(#function) must be called from the main thread! Offending stack trace is:\n\n\(stacktrace)\n" }
+            log.error { "ERROR DispatchQueue.\(#function) must be called from the main thread! Offending stack trace is:\n\n\(stacktrace)\n" }
 		}
 	}
 }
 
+
 /// This lookup map dictionary stores the last submitted worker block for a given identifier string
 
 private var workItems:[String:DispatchWorkItem] = [:]
-
 
 
 //----------------------------------------------------------------------------------------------------------------------
--- BXSwiftUtils/Threading/NSObject+Coalescing.swift
+++ BXSwiftUtils/Threading/NSObject+Coalescing.swift
@@ -16,13 +16,16 @@ import Foundation
 public extension NSObject
 {
 	/**
+
 	Performs a method call after the specified delay. If multiple requests are queued up, the method will be
 	called only once after the delay has elapsed. Please note that both the method selector and the argument
 	need to be the same for coalescing to take effect.
+	
 	- parameter selector: The single argument method to be called
 	- parameter argument: The object argument to this method
 	- parameter delay: This method will only be called after this optional delay has elapsed. If the delay is
 	0.0 it will be called during the next runloop cycle.
+	
 	*/
 	
 	public func performCoalesced(_ selector: Selector, argument: AnyObject?=nil, delay: TimeInterval=0.0)
@@ -32,12 +35,20 @@ public extension NSObject
 	}
 
 
-	/// Cancel a specific outstanding perform request for the specified method and argument. Please note that the
-	/// combination of selector and argument is important here. Using the same selector with a different argument
-	/// will not cancel anything.
-	/// - parameter selector: The method to be canceled
-	/// - parameter argument: The object argument to this method
+//----------------------------------------------------------------------------------------------------------------------
 
+
+	/**
+	
+	Cancel a specific outstanding perform request for the specified method and argument. Please note that the
+	combination of selector and argument is important here. Using the same selector with a different argument
+	will not cancel anything.
+	
+	- parameter selector: The method to be canceled
+	- parameter argument: The object argument to this method
+	
+	*/
+	
 	public func cancelDelayedPerform(_ selector: Selector, argument: AnyObject?=nil)
 	{
 		NSObject.cancelPreviousPerformRequests(withTarget:self, selector:selector, object:argument)
@@ -51,7 +62,10 @@ public extension NSObject
 		NSObject.cancelPreviousPerformRequests(withTarget:self)
 		RunLoop.current.cancelPerformSelectors(withTarget:self)
 	}
-}
 
 
 //----------------------------------------------------------------------------------------------------------------------
+
+
+}
+
