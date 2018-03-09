acd40903a73b0bf7f71f59c4de34a07e8072f3ba
Stefan Fochler <istefo@me.com>
2018-03-09 14:15:47 +0100

    Add iOS Framework

--- BXSwiftUtils/BXSwiftUtils.h
+++ BXSwiftUtils/BXSwiftUtils.h
@@ -6,7 +6,7 @@
 //  Copyright © 2018 Boinx Software Ltd. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for BXSwiftUtils.
 FOUNDATION_EXPORT double BXSwiftUtilsVersionNumber;
