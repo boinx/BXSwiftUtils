cfa27bd636558ec110182c998b3ec792a6b55432
Stefan Fochler <istefo@me.com>
2018-03-12 16:03:45 +0100

    Simplify Synchronized, add tests

--- BXSwiftUtils/Threading/Synchronized.swift
+++ BXSwiftUtils/Threading/Synchronized.swift
@@ -12,79 +12,29 @@ import Foundation
 
 //----------------------------------------------------------------------------------------------------------------------
 
-
 /**
 
-This global functions simulates the @synchronized feature of Objective-C. Use just like in Objective-C,
-but without the @ character:
+ This global functions simulates the @synchronized feature of Objective-C.
+ Use just like in Objective-C, but without the @ character.
+ 
+ The block may also return some value, wich is useful for implementing getter methods:
 
-		synchronized(self)
-		{
-		   ...
-		}
+     synchronized(self)
+     {
+         return _someUnsafeProperty
+     }
 
-- Parameter lock: The object that is used for locking
-- Parameter closure: The closure that is executed when the lock is acquired
-
+ - Parameter lock: The object that is used for locking.
+ - Parameter closure: The closure that is executed when the lock is acquired.
+ - Returns: The return value of the block is returned to the caller.
+ - Throws: Re-throws if the given block throws.
 */
-
-public func synchronized(_ lock: AnyObject, _ closure: ()->() )
+public func synchronized<T>(_ lock: AnyObject, _ closure: () throws -> T) rethrows -> T
 {
     objc_sync_enter(lock)
     defer { objc_sync_exit(lock) }
 
-    closure()
-}
-
-
-/**
-
-This global functions simulates the @synchronized feature of Objective-C. This variant takes a block that
-can throw an error:
-
-		synchronized(self)
-		{
-		   ...
-		}
-		
-- Parameter lock: The object that is used for locking
-- Parameter closure: The closure that is executed when the lock is acquired
-
-*/
-
-public func synchronized(_ lock: AnyObject, _ closure:() throws->() ) rethrows
-{
-    objc_sync_enter(lock)
-    defer { objc_sync_exit(lock) }
-
-    try closure()
-}
-
-
-/**
-
-This global functions simulates the @synchronized feature of Objective-C. Use just like in Objective-C,
-but without the @ character. The main difference here is, that the block returns something. This is useful
-for implementing getter methods:
-
-		synchronized(self)
-		{
-		   return _someUnsafeProperty
-		}
-
-- Parameter lock: The object that is used for locking
-- Parameter closure: The closure that is executed when the lock is acquired
-- Returns: The return value of the block is returned to the caller
-
-*/
-
-public func synchronized<T>(_ lock: AnyObject, _ closure: ()->T ) -> T
-{
-    objc_sync_enter(lock)
-    defer { objc_sync_exit(lock) }
-
-    let result = closure()
-	return result
+    return try closure()
 }
 
 
