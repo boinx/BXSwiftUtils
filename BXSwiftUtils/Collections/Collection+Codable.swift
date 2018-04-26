b9144da19fecb802a54105bab383b4b1ab575006
Peter Baumgartner <peter@boinx.com>
2018-04-26 08:17:17 +0200

    Renamed extension to Collection+Codable.swift and fixed some bugs

--- BXSwiftUtils/Collections/Collection+Codable.swift
+++ BXSwiftUtils/Collections/Collection+Codable.swift
@@ -1,30 +1,27 @@
 //**********************************************************************************************************************
 //
-//  Dictionary+Codable.swift
-//	Adds Codable support for [String:Any] dictionaries
+//  Collection+Codable.swift
+//	Adds Codable support for [String:Any] dictionaries and [Any] arrays
 //  Copyright ©2018 Peter Baumgartner. All rights reserved.
 //
 //**********************************************************************************************************************
 
 
-// The type Dictionary<String,Any> is not Codable by default, because Any does not conform to the Codable protocol.
-// The following extensions add Codable support for [String:Any] and [Any], so that arbitrary dictionary and arrays
-// can be encoded and decoded.
-
-// Inspired by https://stackoverflow.com/questions/44603248/how-to-decode-a-property-with-type-of-json-dictionary-in-swift-4-decodable-proto
-// and https://stackoverflow.com/questions/47575309/how-to-encode-a-property-with-type-of-json-dictionary-in-swift-4-encodable-proto
-
-
-//----------------------------------------------------------------------------------------------------------------------
-
-
 import Foundation
 
 
+// The types Dictionary<String,Any> Array<Any> are not Codable by default, because Any does not conform to the
+// Codable protocol. The following extensions add Codable support for [String:Any] and [Any], so that arbitrary
+// dictionary and arrays can be encoded and decoded. Inspired by:
+
+// https://stackoverflow.com/questions/47575309/how-to-encode-a-property-with-type-of-json-dictionary-in-swift-4-encodable-proto
+// https://stackoverflow.com/questions/44603248/how-to-decode-a-property-with-type-of-json-dictionary-in-swift-4-decodable-proto
+
+
 //----------------------------------------------------------------------------------------------------------------------
 
 
-fileprivate struct DictionaryCodingKey : CodingKey
+fileprivate struct DynamicCodingKey : CodingKey
 {
     var stringValue: String
     var intValue: Int?
@@ -49,52 +46,6 @@ fileprivate struct DictionaryCodingKey : CodingKey
 
 extension KeyedEncodingContainer
 {
-
-	/// Encodes a dictionary of type [String:Any]
-	
-	public mutating func encode(_ dict: [String:Any], forKey key: Key) throws
-	{
-  		var container = self.nestedContainer(keyedBy: DictionaryCodingKey.self, forKey: key)
-
-		for (k,v) in dict
-		{
-			guard let key = DictionaryCodingKey(stringValue:k) else { continue }
-			
-			if let value = v as? Double
-			{
-				try container.encode(value, forKey: key)
-			}
-			else if let value = v as? Float
-			{
-				try container.encode(value, forKey: key)
-			}
-			else if let value = v as? Int
-			{
-				try container.encode(value, forKey: key)
-			}
-			else if let value = v as? Bool
-			{
-				try container.encode(value, forKey: key)
-			}
-			else if let value = v as? Date
-			{
-				try container.encode(value, forKey: key)
-			}
-			else if let value = v as? String
-			{
-				try container.encode(value, forKey: key)
-			}
-			else if let value = v as? [Any]
-			{
-				try container.encode(value, forKey: key)
-			}
-			else if let value = v as? [String:Any]
-			{
-				try container.encode(value, forKey: key)
-			}
-		}
-	}
-
 
 	/// Encodes an array of type [Any]
 
@@ -136,95 +87,26 @@ extension KeyedEncodingContainer
 			{
 				try container.encode(value)
 			}
+			else
+			{
+				throw EncodingError.invalidValue(v, EncodingError.Context(
+					codingPath:container.codingPath,
+					debugDescription:"The type \(type(of:v)) is not supported",
+					underlyingError:nil))
+			}
 		}
 	}
-	
-	
+
+
 	/// Encodes a dictionary of type [String:Any]
 	
-	public mutating func encodeIfPresent(_ value: [String:Any]?, forKey key: Key) throws
+	public mutating func encode(_ dict: [String:Any], forKey key: Key) throws
 	{
-		if let value = value
-		{
-			try self.encode(value, forKey: key)
-		}
-	}
-
-
-	/// Encodes an array of type [Any]
-	
-	public mutating func encodeIfPresent(_ value: [Any]?, forKey key: Key) throws
-	{
-		if let value = value
-		{
-			try self.encode(value, forKey: key)
-		}
-	}
-
-}
-
-
-//----------------------------------------------------------------------------------------------------------------------
-
-
-// MARK: -
-
-extension UnkeyedEncodingContainer
-{
-
-	/// Encodes an array of type [Any]
-
-	public mutating func encode(_ array: [Any]) throws
-	{
- 		var container = self.nestedUnkeyedContainer()
-
-		for value in array
-		{
-			if let value = value as? Double
-			{
-				try container.encode(value)
-			}
-			else if let value = value as? Float
-			{
-				try container.encode(value)
-			}
-			else if let value = value as? Int
-			{
-				try container.encode(value)
-			}
-			else if let value = value as? Bool
-			{
-				try container.encode(value)
-			}
-			else if let value = value as? Date
-			{
-				try container.encode(value)
-			}
-			else if let value = value as? String
-			{
-				try container.encode(value)
-			}
-			else if let value = value as? [Any]
-			{
-				try container.encode(value)
-			}
-			else if let value = value as? [String:Any]
-			{
-				try container.encode(value)
-			}
-		}
-	}
-	
-	
-	/// Encodes a dictionary of type [String:Any]
-	
-	public mutating func encode(_ dict: [String:Any]) throws
-	{
-        var container = self.nestedContainer(keyedBy: DictionaryCodingKey.self)
+  		var container = self.nestedContainer(keyedBy: DynamicCodingKey.self, forKey: key)
 		
 		for (k,v) in dict
 		{
-			guard let key = DictionaryCodingKey(stringValue:k) else { continue }
+			guard let key = DynamicCodingKey(stringValue:k) else { continue }
 			
 			if let value = v as? Double
 			{
@@ -258,8 +140,153 @@ extension UnkeyedEncodingContainer
 			{
 				try container.encode(value, forKey: key)
 			}
+			else
+			{
+				throw EncodingError.invalidValue(v, EncodingError.Context(
+					codingPath:container.codingPath,
+					debugDescription:"The type \(type(of:v)) is not supported",
+					underlyingError:nil))
+			}
+		}
+	}
+
+
+	/// Encodes an array of type [Any]
+	
+	public mutating func encodeIfPresent(_ value: [Any]?, forKey key: Key) throws
+	{
+		if let value = value
+		{
+			try self.encode(value, forKey: key)
+		}
+	}
+
+	
+	/// Encodes a dictionary of type [String:Any]
+	
+	public mutating func encodeIfPresent(_ value: [String:Any]?, forKey key: Key) throws
+	{
+		if let value = value
+		{
+			try self.encode(value, forKey: key)
+		}
+	}
+
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+// MARK: -
+
+extension UnkeyedEncodingContainer
+{
+
+	/// Encodes an array of type [Any]
+
+	public mutating func encode(_ array: [Any]) throws
+	{
+ 		var container = self.nestedUnkeyedContainer()
+
+		for v in array
+		{
+			if let value = v as? Double
+			{
+				try container.encode(value)
+			}
+			else if let value = v as? Float
+			{
+				try container.encode(value)
+			}
+			else if let value = v as? Int
+			{
+				try container.encode(value)
+			}
+			else if let value = v as? Bool
+			{
+				try container.encode(value)
+			}
+			else if let value = v as? Date
+			{
+				try container.encode(value)
+			}
+			else if let value = v as? String
+			{
+				try container.encode(value)
+			}
+			else if let value = v as? [Any]
+			{
+				try container.encode(value)
+			}
+			else if let value = v as? [String:Any]
+			{
+				try container.encode(value)
+			}
+			else
+			{
+				throw EncodingError.invalidValue(v, EncodingError.Context(
+					codingPath:container.codingPath,
+					debugDescription:"The type \(type(of:v)) is not supported",
+					underlyingError:nil))
+			}
+		}
+	}
+
+
+	/// Encodes a dictionary of type [String:Any]
+	
+	public mutating func encode(_ dict: [String:Any]) throws
+	{
+        var container = self.nestedContainer(keyedBy: DynamicCodingKey.self)
+		
+		for (k,v) in dict
+		{
+			guard let key = DynamicCodingKey(stringValue:k) else { continue }
+			
+			if let value = v as? Double
+			{
+				try container.encode(value, forKey: key)
+			}
+			else if let value = v as? Float
+			{
+				try container.encode(value, forKey: key)
+			}
+			else if let value = v as? Int
+			{
+				try container.encode(value, forKey: key)
+			}
+			else if let value = v as? Bool
+			{
+				try container.encode(value, forKey: key)
+			}
+			else if let value = v as? Date
+			{
+				try container.encode(value, forKey: key)
+			}
+			else if let value = v as? String
+			{
+				try container.encode(value, forKey: key)
+			}
+			else if let value = v as? [Any]
+			{
+				try container.encode(value, forKey: key)
+			}
+			else if let value = v as? [String:Any]
+			{
+				try container.encode(value, forKey: key)
+			}
+			else
+			{
+				throw EncodingError.invalidValue(v, EncodingError.Context(
+					codingPath:container.codingPath,
+					debugDescription:"The type \(type(of:v)) is not supported",
+					underlyingError:nil))
+			}
 		}
     }
+
+	
 	/// Encodes an array of type [Any]
 
 	public mutating func encodeIfPresent(_ array: [Any]?) throws
@@ -269,7 +296,7 @@ extension UnkeyedEncodingContainer
 			try self.encode(array)
 		}
 	}
-	
+
 	
 	/// Encodes a dictionary of type [String:Any]
 	
@@ -292,11 +319,69 @@ extension UnkeyedEncodingContainer
 extension KeyedDecodingContainer
 {
 
+	/// Decodes an array of type [Any]
+	
+	public func decode(_ type: [Any].Type, forKey key: Key) throws -> [Any]
+	{
+        var container = try self.nestedUnkeyedContainer(forKey: key)
+		var array:[Any] = []
+
+		while !container.isAtEnd
+		{
+			if try container.decodeNil()
+			{
+                continue
+            }
+			else if let value = try? container.decode(Double.self)
+			{
+				array += value
+			}
+			else if let value = try? container.decode(Float.self)
+			{
+				array += value
+			}
+			else if let value = try? container.decode(Int.self)
+			{
+				array += value
+			}
+			else if let value = try? container.decode(Bool.self)
+			{
+				array += value
+			}
+			else if let value = try? container.decode(Date.self)
+			{
+				array += value
+			}
+			else if let value = try? container.decode(String.self)
+			{
+				array += value
+			}
+			else if let value = try? container.decode([Any].self)
+			{
+				array += value
+			}
+			else if let value = try? container.decode([String:Any].self)
+			{
+				array += value
+			}
+			else
+			{
+//				throw DecodingError.dataCorrupted(DecodingError.Context(
+//					codingPath:container.codingPath,
+//					debugDescription:"Collection contains unsupported type",
+//					underlyingError:nil))
+			}
+		}
+		
+		return array
+    }
+
+
 	/// Decodes a dictionary of type [String:Any]
 	
 	public func decode(_ type: [String:Any].Type, forKey key: Key) throws -> [String:Any]
 	{
-  		let container = try self.nestedContainer(keyedBy: DictionaryCodingKey.self, forKey: key)
+  		let container = try self.nestedContainer(keyedBy: DynamicCodingKey.self, forKey: key)
 		var dictionary:[String:Any] = [:]
 
 		for k in container.allKeys
@@ -333,6 +418,13 @@ extension KeyedDecodingContainer
 			{
 				dictionary[k.stringValue] = value
 			}
+			else
+			{
+//				throw DecodingError.dataCorrupted(DecodingError.Context(
+//					codingPath:container.codingPath,
+//					debugDescription:"Collection contains unsupported type",
+//					underlyingError:nil))
+			}
 		}
 		
 		return dictionary
@@ -341,10 +433,11 @@ extension KeyedDecodingContainer
 
 	/// Decodes an array of type [Any]
 	
-	public func decode(_ type: [Any].Type, forKey key: Key) throws -> [Any]
+	public func decodeIfPresent(_ type: [Any].Type, forKey key: Key) throws -> [Any]?
 	{
-        var container = try self.nestedUnkeyedContainer(forKey: key)
-        return try container.decode(type)
+        guard contains(key) else { return nil }
+        guard try decodeNil(forKey: key) == false else { return nil }
+        return try decode(type, forKey: key)
     }
 
 
@@ -358,16 +451,6 @@ extension KeyedDecodingContainer
 	}
 	
 	
-	/// Decodes an array of type [Any]
-	
-	public func decodeIfPresent(_ type: [Any].Type, forKey key: Key) throws -> [Any]?
-	{
-        guard contains(key) else { return nil }
-        guard try decodeNil(forKey: key) == false else { return nil }
-        return try decode(type, forKey: key)
-    }
-
-
 }
 
 
@@ -383,45 +466,53 @@ extension UnkeyedDecodingContainer
 
 	public mutating func decode(_ type: [Any].Type) throws -> [Any]
 	{
+        var container = try self.nestedUnkeyedContainer()
 		var array:[Any] = []
 
-		while !self.isAtEnd
+		while !container.isAtEnd
 		{
-			if try decodeNil()
+			if try container.decodeNil()
 			{
                 continue
             }
-			else if let value = try? decode(Double.self)
+			else if let value = try? container.decode(Double.self)
 			{
 				array += value
 			}
-			else if let value = try? decode(Float.self)
+			else if let value = try? container.decode(Float.self)
 			{
 				array += value
 			}
-			else if let value = try? decode(Int.self)
+			else if let value = try? container.decode(Int.self)
 			{
 				array += value
 			}
-			else if let value = try? decode(Bool.self)
+			else if let value = try? container.decode(Bool.self)
 			{
 				array += value
 			}
-			else if let value = try? decode(Date.self)
+			else if let value = try? container.decode(Date.self)
 			{
 				array += value
 			}
-			else if let value = try? decode(String.self)
+			else if let value = try? container.decode(String.self)
 			{
 				array += value
 			}
-			else if let value = try? decode([Any].self)
+			else if let value = try? container.decode([Any].self)
 			{
 				array += value
 			}
-			else if let value = try? decode([String:Any].self)
+			else if let value = try? container.decode([String:Any].self)
 			{
 				array += value
+			}
+			else
+			{
+//				throw DecodingError.dataCorrupted(DecodingError.Context(
+//					codingPath:container.codingPath,
+//					debugDescription:"Collection contains unsupported type",
+//					underlyingError:nil))
 			}
 		}
 		
@@ -433,7 +524,7 @@ extension UnkeyedDecodingContainer
 	
 	public mutating func decode(_ type: [String:Any].Type) throws -> [String:Any]
 	{
-        let container = try self.nestedContainer(keyedBy: DictionaryCodingKey.self)
+        let container = try self.nestedContainer(keyedBy: DynamicCodingKey.self)
 		var dictionary:[String:Any] = [:]
 
 		for k in container.allKeys
@@ -470,6 +561,13 @@ extension UnkeyedDecodingContainer
 			{
 				dictionary[k.stringValue] = value
 			}
+			else
+			{
+//				throw DecodingError.dataCorrupted(DecodingError.Context(
+//					codingPath:container.codingPath,
+//					debugDescription:"Collection contains unsupported type",
+//					underlyingError:nil))
+			}
 		}
 		
 		return dictionary
@@ -484,7 +582,7 @@ extension UnkeyedDecodingContainer
 	}
 	
 	
-	/// Decodes an array of type [Any]
+	/// Decodes a dictionary of type [String:Any]
 
 	public mutating func decodeIfPresent(_ type: [String:Any].Type) throws -> [String:Any]?
 	{
