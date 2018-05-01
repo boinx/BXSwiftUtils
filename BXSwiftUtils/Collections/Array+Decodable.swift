8feec4dab48dd8531a7b1e371a6d26c86a60ff49
Stefan Fochler <istefo@me.com>
2018-05-01 11:12:09 +0200

    Fix availability of Array+Decodable

--- BXSwiftUtils/Collections/Array+Decodable.swift
+++ BXSwiftUtils/Collections/Array+Decodable.swift
@@ -14,7 +14,6 @@ extension KeyedDecodingContainer
 	/// This method decodes a heterogeneous array of T. Since the array can contain arbitrary subclass instances
 	/// of T, the objectTypeMapper closure is used to determine the exact subclass type of each element in the array.
     
-    @available(swift 4.1)
 	public func decodeHeterogeneousArray<T: Decodable>(forKey key: Key, objectTypeMapper: (Decoder) throws -> T.Type) throws -> [T]
 	{
 		var arrayContainer = try self.nestedUnkeyedContainer(forKey:key)
