61de981693266785fdcb77151bdce547a04124cf
Peter Baumgartner <peter@boinx.com>
2018-05-12 15:55:57 +0200

    Improved implementation

--- BXSwiftUtils/Threading/DispatchQueue+Throttle.swift
+++ BXSwiftUtils/Threading/DispatchQueue+Throttle.swift
@@ -22,28 +22,33 @@ extension DispatchQueue
 	
 	private static var throttledWorkTimes: [String:CFAbsoluteTime] = [:]
 
-	// A lock that protects against threading issue when accessing the two shared lookup dictionaries above
+	// A lock that protects against threading issue when accessing the two shared dictionaries above
 	
-	private static var lock:BXReadWriteLock = BXReadWriteLock(label:"com.boinx.DispatcherItems")
-	
+	private static var lock:BXReadWriteLock = BXReadWriteLock(label:"com.boinx.BXSwiftUtils.DispatchQueue.lock")
+
 
 //----------------------------------------------------------------------------------------------------------------------
 
 
 	// MARK: -
+	
 
-	/// Coalesces all blocks with the same identifier to the end of the specified interval. The block
-	/// will only be executed once, when no calls occur for at least the specified interval time.
-	/// ````
-	/// DispatchQueue.main.coalesce("performExpensiveWork",interval:0.1)
-	/// {
-	///     performExpensiveWork()
-	/// }
-	/// ````
-	/// - parameter identifier: A unique identifier string that is used to coalesce block invocations together
-	/// - parameter interval: The time in seconds before the block is first executed
-	/// - parameter block: The closure (block) to be executed
+	/**
+	
+	Coalesces all blocks with the same identifier to the end of the specified interval. The block
+	will only be executed once, when no calls occur for at least the specified interval time.
+	````
+	DispatchQueue.main.coalesce("performExpensiveWork",interval:0.1)
+	{
+	    performExpensiveWork()
+	}
+	````
+	- parameter identifier: A unique identifier string that is used to coalesce block invocations together
+	- parameter interval: The time in seconds before the block is first executed
+	- parameter block: The closure (block) to be executed
 
+	*/
+	
 	public func coalesce(_ identifier: String, interval: CFTimeInterval=0.0, block: @escaping ()->Void)
 	{
 		DispatchQueue.lock.read()
@@ -58,7 +63,10 @@ extension DispatchQueue
  			DispatchQueue.lock.write() { DispatchQueue.coalescedWorkItems[identifier] = nil }
  		}
 		
-		DispatchQueue.lock.write() { DispatchQueue.coalescedWorkItems[identifier] = newItem }
+		DispatchQueue.lock.write()
+		{
+			DispatchQueue.coalescedWorkItems[identifier] = newItem
+		}
 		
         self.asyncAfter(deadline: .now()+interval, execute: newItem)
 	}
@@ -67,38 +75,43 @@ extension DispatchQueue
 //----------------------------------------------------------------------------------------------------------------------
 
 
-	/// Throttles all blocks with the same identifier to be called only once (at the beginnging) of the specified
-	/// interval. All remaining calls with the same identifier will be suppressed until the interval has elapsed.
-	/// ````
-	/// DispatchQueue.main.throttle("performExpensiveWork",interval:0.1)
-	/// {
-	///     performExpensiveWork()
-	/// }
-	/// ````
-	/// - parameter identifier: A unique identifier string that is used to group block invocations together
-	/// - parameter interval: The interval duration (in seconds) between block invocations
-	/// - parameter block: The closure (block) to be executed
+	/**
+	
+	Throttles all blocks with the same identifier to be called only once (at the beginnging) of the specified
+	interval. All remaining calls with the same identifier will be suppressed until the interval has elapsed.
+	````
+	DispatchQueue.main.throttle("performExpensiveWork",interval:0.1)
+	{
+	    performExpensiveWork()
+	}
+	````
+	- parameter identifier: A unique identifier string that is used to group block invocations together
+	- parameter interval: The interval duration (in seconds) between block invocations
+	- parameter block: The closure (block) to be executed
+
+	*/
 
 	public func throttle(_ identifier: String, interval: CFTimeInterval=0.0, block: @escaping ()->Void)
 	{
-		let item = DispatchWorkItem()
-		{
- 			DispatchQueue.lock.write()
- 			{
- 				DispatchQueue.throttledWorkTimes[identifier] = CFAbsoluteTimeGetCurrent()
- 				block()
- 			}
-		}
-
+		var shouldExecuteBlock = false
+		
 		DispatchQueue.lock.read()
 		{
 			let lastTime = DispatchQueue.throttledWorkTimes[identifier] ?? 0.0
 			let now = CFAbsoluteTimeGetCurrent()
-			
-			if now >= lastTime+interval
+			shouldExecuteBlock = now >= lastTime+interval
+		}
+		
+		if shouldExecuteBlock
+		{
+			let item = DispatchWorkItem()
 			{
-				self.async(execute:item)
+				let time = CFAbsoluteTimeGetCurrent()
+				block()
+				DispatchQueue.lock.write() { DispatchQueue.throttledWorkTimes[identifier] = time }
 			}
+
+			self.async(execute:item)
 		}
 	}
 
@@ -122,7 +135,9 @@ extension DispatchQueue
 	}
 	
 
-	/// Cancels all scheduled coalesced work with identifiers that have a matching prefix.
+	/// Cancels all scheduled coalesced work with identifiers that have a matching prefix. This is useful
+	/// for cleaning up pending work for a controller that is about to be deallocated. It is obviously the
+	/// responsibility of the caller to give all this work identifiers with matching prefixes.
 	
 	public static func cancelCoalescedWork(withPrefix prefix: String)
 	{
@@ -158,5 +173,7 @@ extension DispatchQueue
 
 
 //----------------------------------------------------------------------------------------------------------------------
+
+
 
 
