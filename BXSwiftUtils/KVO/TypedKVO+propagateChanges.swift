cd9d8e88f6292f128a46673ec2ba125fe34f6166
Stefan Fochler <istefo@me.com>
2018-06-25 14:41:46 +0200

    Add documentation to propagateChanges

--- BXSwiftUtils/KVO/TypedKVO+propagateChanges.swift
+++ BXSwiftUtils/KVO/TypedKVO+propagateChanges.swift
@@ -10,6 +10,16 @@ import Foundation
 
 extension TypedKVO
 {
+    /**
+     Setup a "link" between `origin`'s property at `fromKeyPath` to `target`'s property at `toKeyPath` by simulating an
+     update to `toKeyPath` using the willChangeValue/didChangeValue pair whenever the value at `fromKeyPath` fires.
+     This can be used to link two properties in situations where KVO dependent keys are not usable (e.g. when the
+     dependent key resides on an NSArrayController) or the mechanism is not fine-grained enough.
+     
+     The target object is not retained.
+     
+     There is no initial call on creation.
+     */
     public static func propagateChanges<Target, TargetValue>(from origin: Root, _ fromKeyPath: KeyPath<Root, Value>, to target: Target, _ toKeyPath: KeyPath<Target, TargetValue>, asyncOn queue: DispatchQueue? = nil) -> TypedKVO where Target: NSObject
     {
         let notify = { [weak target] in
