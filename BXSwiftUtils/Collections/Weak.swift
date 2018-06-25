c3ab741be62481b1cb33b997aec4694c9e093753
Stefan Fochler <istefo@me.com>
2018-06-25 14:36:03 +0200

    Add Weak

---
+++ BXSwiftUtils/Collections/Weak.swift
@@ -0,0 +1,25 @@
+//
+//  Weak.swift
+//  BXSwiftUtils
+//
+//  Created by Stefan Fochler on 25.06.18.
+//  Copyright © 2018 Boinx Software Ltd. & Imagine GbR. All rights reserved.
+//
+
+import Foundation
+
+/**
+ This class defines a simple box that stores a weak reference to a value type.
+ 
+ This can be handy when creating weak colletions in Swift that should not hold a strong reference to the elements, or
+ when used with APIs that would otherwise create a strong reference to their argument.
+ */
+public class Weak<Value> where Value: AnyObject
+{
+    public private(set) weak var ref: Value?
+    
+    public init(_ ref: Value)
+    {
+        self.ref = ref
+    }
+}
