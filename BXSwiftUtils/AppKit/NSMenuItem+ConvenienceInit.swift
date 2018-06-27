df6d87a884b1d35384b372b346a3a1ca7c07a820
Stefan Fochler <istefo@me.com>
2018-06-27 10:52:51 +0200

    Add NSMenuItem+ConvenienceInit and NSMenu+Concatenation

---
+++ BXSwiftUtils/AppKit/NSMenuItem+ConvenienceInit.swift
@@ -0,0 +1,36 @@
+//
+//  NSMenuItem+EnumCaseInit.swift
+//  BXSwiftUtils
+//
+//  Created by Stefan Fochler on 27.06.18.
+//  Copyright © 2018 Boinx Software Ltd. & Imagine GbR. All rights reserved.
+//
+
+import AppKit
+
+extension NSMenuItem
+{
+    /**
+     Convenience initializer that creates a menu item with a given `title` and `value` which will be used as the item's
+     `representedObject`.
+     
+     - parameter title: The item's displayable title.
+     - parameter value: The value that is represented by this item.
+     */
+    public convenience init(title: String, value: Any)
+    {
+        self.init(title: title, action: nil, keyEquivalent: "")
+        self.representedObject = value
+    }
+    
+    /**
+     Convenience initializer that creates a menu item with the given `value` used as the item's `representedObject`.
+     The item's `title` will be set to `value`'s `rawValue`, which must be a `String`.
+     
+     - parameter value: The value that is represented by this item and the raw value of which must be a String.
+     */
+    public convenience init<Value>(for value: Value) where Value: RawRepresentable, Value.RawValue == String
+    {
+        self.init(title: value.rawValue, value: value)
+    }
+}
