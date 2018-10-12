61ef7f258f01132108d914202c58154b48a46d2e
Peter Baumgartner <peter@boinx.com>
2018-10-12 11:54:00 +0200

    Added new String extension for simple regex support

---
+++ BXSwiftUtils/Strings/String+Regex.swift
@@ -0,0 +1,53 @@
+//**********************************************************************************************************************
+//
+//  String+Regex.swift
+//	Simple regex functions
+//  Copyright ©2018 Peter Baumgartner. All rights reserved.
+//
+//**********************************************************************************************************************
+
+
+import Foundation
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+// MARK: -
+
+public extension String
+{
+
+	/// Returns any matches for the supplied regex pattern
+	/// - parameter pattern: A valid regex pattern
+	/// - parameter options: The options inluence the behavior of the NSRegularExpression
+	/// - returns: An array of strings matching the regex pattern
+	
+	public func regexMatches(for pattern: String, options: NSRegularExpression.Options = []) -> [String]
+	{
+ 		if let regex = try? NSRegularExpression(pattern:pattern, options:options)
+        {
+            let string = self as NSString
+			let range = NSRange(location:0, length:string.length)
+			
+            return regex.matches(in:self, options:[], range:range).map
+            {
+                string.substring(with: $0.range)
+            }
+        }
+
+        return []
+	}
+	
+	
+	/// Returns true if a string has at least one match for the supplied regex pattern
+	
+	public static func ~= (_ string: String, _ pattern: String) -> Bool
+	{
+		return !string.regexMatches(for:pattern).isEmpty
+	}
+	
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
---
+++ BXSwiftUtilsTests/Strings/String+RegexTests.swift
@@ -0,0 +1,42 @@
+//
+//  String+UniqueByIncrementingTests.swift
+//  BXSwiftUtils
+//
+//  Created by Peter Baumgartner on 12.10.18.
+//  Copyright © 2018 Boinx Software Ltd. & Imagine GbR. All rights reserved.
+//
+
+import XCTest
+import BXSwiftUtils
+
+class String_RegexTests: XCTestCase
+{
+
+     func testPositive()
+    {
+    	let str = "Bla Laber ABC Schwafel Sülz"
+    	let matches = str.regexMatches(for:"ABC")
+        XCTAssertEqual(matches,["ABC"])
+    }
+	
+   func testNegative()
+    {
+    	let str = "Bla Laber ABC Schwafel Sülz"
+    	let matches = str.regexMatches(for:"XYZ")
+        XCTAssertEqual(matches,[])
+    }
+    
+    func testMatchCount()
+    {
+    	let str = "Bla ABC Laber ABC Schwafel ABC Sülz"
+    	let matches = str.regexMatches(for:"ABC")
+        XCTAssertEqual(matches.count,3)
+    }
+	
+    func testOperator()
+    {
+    	let str = "Bla Laber ABC Schwafel Sülz"
+    	let matches = str ~= "ABC"
+        XCTAssertEqual(matches,true)
+    }
+}
