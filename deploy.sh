08c1d932303ca8c47945cff63c1f06238f51ce11
Stefan Fochler <istefo@me.com>
2018-10-15 15:06:42 +0200

    Fix typo in macOS framework

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ set -o pipefail; xcodebuild -scheme "BXSwiftUtils-macOS" -configuration "Release
 source .bx_build_env
 
 pushd "$BUILD_PRODUCTS_DIR"
-zip -ryq "$SRCROOT/$_artifacts/BXSwiftUtils-macoS.framework.zip" "$PRODUCT_NAME"
+zip -ryq "$SRCROOT/$_artifacts/BXSwiftUtils-macOS.framework.zip" "$PRODUCT_NAME"
 popd
 
 set -o pipefail; xcodebuild -scheme "BXSwiftUtils-iOS" -configuration "Release" clean build | xcpretty
@@ -22,16 +22,3 @@ pushd "$BUILD_PRODUCTS_DIR"
 zip -ryq "$SRCROOT/$_artifacts/BXSwiftUtils-iOS.framework.zip" "$PRODUCT_NAME"
 popd
 
-# Delete git tag if already existing
-#git remote add origin-authenticated "https://${GH_TOKEN}@github.com/boinx/BXSwiftUtils.git"
-#
-## if deleting the tag fails, that's ok
-#set +e
-#_git_tag="travis"
-#git tag -d ${_git_tag}
-#git push origin-authenticated :refs/tags/${_git_tag}
-#set -e
-
-# Create new "latest" tag
-#git tag -f -m "Travis tagged with ${_git_tag}" -a "${_git_tag}"
-#git push --tags origin-authenticated master
