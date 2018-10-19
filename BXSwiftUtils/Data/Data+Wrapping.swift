3afbd530e4498de8d2d0182f34f5c397a6a10729
Peter Baumgartner <peter@boinx.com>
2018-10-19 12:20:43 +0200

    Added new Data+Wrapping extension

---
+++ BXSwiftUtils/Data/Data+Wrapping.swift
@@ -0,0 +1,46 @@
+//**********************************************************************************************************************
+//
+//  Data+Wrapping.swift
+//  Adds methods to convert between Array and Data
+//  Copyright ©2018 Peter Baumgartner. All rights reserved.
+//
+//**********************************************************************************************************************
+
+
+import Foundation
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+public extension Data
+{
+
+	/// Wraps an Array in a Data object WITHOUT copying the underlying memory
+	/// - parameter array: An array of values of type T
+	
+	init<T>(wrapping array: inout [T])
+    {
+		let ptr = UnsafeMutableRawPointer(&array)
+		let count = array.count * MemoryLayout<T>.stride
+        self.init(bytesNoCopy:ptr, count:count, deallocator:.none)
+    }
+
+
+	/// Converts a Data object to an Array WITHOUT copying the underlying memory
+	/// - parameter elementType: The type of the array elements
+
+    func asArray<T>(elementType: T.Type) -> [T]
+    {
+    	let count = self.count / MemoryLayout<T>.stride
+		
+        return self.withUnsafeBytes
+        {
+            [T](UnsafeBufferPointer(start:$0, count:count))
+        }
+    }
+
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
