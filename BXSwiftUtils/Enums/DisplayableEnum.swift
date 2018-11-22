c818ed4426f44cb1e21ec10f92eb21e206620e21
Peter Baumgartner <peter@boinx.com>
2018-11-22 08:11:23 +0100

    Added DisplayableEnum protocol

---
+++ BXSwiftUtils/Enums/DisplayableEnum.swift
@@ -0,0 +1,66 @@
+//**********************************************************************************************************************
+//
+//  DisplayableEnum.swift
+//	Provides an automatic data source for enums in user interfaces like tableviews
+//  Copyright ©2018 Peter Baumgartner. All rights reserved.
+//
+//**********************************************************************************************************************
+
+
+public protocol DisplayableEnum : CaseIterable,Equatable
+{
+	/// Returns the number of possible values
+	
+	static var count: Int { get }
+	
+	/// Returns the value at the given index
+	
+	static func value(at index: Int) -> Self
+	
+	// Returns the index of a given value
+	
+	static func index(of value: Self) -> Int
+	
+	/// Returns the localized name at the given index
+	
+	static func localizedName(at index: Int) -> String
+
+	/// Returns a localized name of the value
+	
+	var localizedName: String { get }
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+public extension DisplayableEnum
+{
+	public static var count: Int
+	{
+		return self.allCases.count
+	}
+
+	public static func value(at index: Int) -> Self
+	{
+		return Array(self.allCases)[index]
+	}
+
+	public static func index(of value: Self) -> Int
+	{
+		return Array(self.allCases).firstIndex(of:value)!
+	}
+
+	public static func localizedName(at index: Int) -> String
+	{
+		return self.value(at: index).localizedName
+	}
+	
+	public var localizedName:String
+	{
+		return "\(self)"
+	}
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
