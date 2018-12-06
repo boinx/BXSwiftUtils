20f0c173f97b273a9ce7a873345c145538c8bc9e
Peter Baumgartner <peter@boinx.com>
2018-12-06 14:32:42 +0100

    Added metod for logging CMTime values

---
+++ BXSwiftUtils/Date & Time/CMTime+Logging.swift
@@ -0,0 +1,33 @@
+//**********************************************************************************************************************
+//
+//  CMTime+Logging.swift
+//	Adds logging description to CMTime
+//  Copyright ©2016 Peter Baumgartner. All rights reserved.
+//
+//**********************************************************************************************************************
+
+
+import CoreMedia
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+extension CMTime
+{
+
+	/// Returns a string that is suitable for logging
+	
+	public var debugDescription : String
+	{
+		let value = self.value
+		let timescale = self.timescale
+		return "CMTime(\(value),\(timescale))"
+	}
+
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
