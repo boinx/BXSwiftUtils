3d5ded87439d98690af6f7b50539df69ecec5f9b
Peter Baumgartner <peter@boinx.com>
2018-12-21 18:33:07 +0100

    New mixin extension for simplifyling OSSignpost usage

---
+++ BXSwiftUtils/Logging/BXSignpostMixin.swift
@@ -0,0 +1,76 @@
+//**********************************************************************************************************************
+//
+//  BXSignpostMixin.swift
+//	Convenience function for using OSSignpost
+//  Copyright ©2018 Peter Baumgartner. All rights reserved.
+//
+//**********************************************************************************************************************
+
+
+import os.signpost
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+/// OSLog for signpost measuring
+
+let signpostlog:OSLog =
+{
+	let identifier = Bundle.main.bundleIdentifier ?? "com.boinx.BXSwiftUtils"
+	return OSLog(subsystem:identifier, category:identifier)
+}()
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+public protocol BXSignpostMixin : class
+{
+
+}
+
+
+extension BXSignpostMixin
+{
+
+	public static func createSignpostLog(withName name:String)
+	{
+	}
+	/// Begins a signpost for measuring the execution time of a function
+	/// - parameter name: The name of the calling class
+	/// - parameter function: the name of the calling function
+	/// - returns: An identifier that can be passed to the endSignpost() function
+	
+	public func beginSignpost(in name:StaticString,_ function:String = #function) -> Any?
+	{
+		if #available(iOS 12.0,*)
+		{
+			let signpostID = OSSignpostID(log:signpostlog)
+			os_signpost(.begin, log:signpostlog, name:name, signpostID:signpostID, "%@.begin",function)
+			return signpostID as Any
+		}
+		
+		return nil
+	}
+
+
+	/// Ends a signpost for measuring the execution time of a function
+	/// - parameter identifier: The identifier that was returned by the beginSignpost() function
+	/// - parameter name: The name of the calling class
+	/// - parameter function: the name of the calling function
+	
+	public func endSignpost(_ identifier:Any?,in name:StaticString,_ function:String = #function)
+	{
+		if #available(iOS 12.0,*)
+		{
+			guard let signpostID = identifier as? OSSignpostID else { return }
+			os_signpost(.end, log:signpostlog, name:name, signpostID:signpostID, "%@.end",function)
+		}
+	}
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
