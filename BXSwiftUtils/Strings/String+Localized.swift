93d804edd49e3dcfe644dfe12b6b49257e4988ef
Peter Baumgartner <peter@boinx.com>
2019-01-30 12:01:14 +0100

    Improved implementation of String.localized() with optional tableSuffix

--- BXSwiftUtils/Strings/String+Localized.swift
+++ BXSwiftUtils/Strings/String+Localized.swift
@@ -21,34 +21,42 @@ public extension String
 	Returns a localized string for the key contained in the receiver (self). If this key hasn't been localized
 	in the strings files yet, then it will show up as the uppercased key enclosed in double angle brackets << KEY >>
 	
-	- Parameter tableName: The name of the .strings file. Provide nil to use the default Localized.strings.
-	- Parameter comment: Provide help to the translator by explaining the context where this string is being used.
-	- Parameter bundle: Provide nil to use the default Bundle.main().
-	- Parameter value: The default string in the development language.
-	- Returns: The localized string (or << KEY >> if the string isn't localized yet
+	- Parameter tableName: The name of the .strings file. Provide nil to use the default Localized.strings
+	- Parameter tableSuffix: An optional suffix that get appended to the tableName
+	- Parameter comment: Provide help to the translator by explaining the context where this string is being used
+	- Parameter bundle: Provide nil to use the default Bundle.main()
+	- Parameter value: The default string in the development language
+	- Returns: The localized string (or << KEY >> if the string isn't localized yet)
 	
 	*/
 	
-    public func localized(from tableName: String?=nil, bundle: Bundle?=nil, comment: String="", value: String?=nil) -> String
+    public func localized(from tableName: String?=nil, tableSuffix: String?="-custom", bundle: Bundle?=nil, comment: String="", value: String?=nil) -> String
 	{
 		let defaultName = self.uppercased()
 		let defaultValue = value ?? "<< \(defaultName) >>"
 		let bundle = bundle ?? Bundle.main
 		
-		var str = NSLocalizedString(
-			self,
-			tableName: tableName,
-			bundle: bundle,
-			value: defaultValue,
-			comment: comment)
+		// If tableName and tableSuffix are both speficied then look there first to save time
 		
-		// Fallback: If the string could not be found in the specified table, then try again with "tableName-custom"
+		var str = defaultValue
 		
-		if str == defaultValue, let tableName = tableName
+		if let tableName = tableName, let tableSuffix = tableSuffix
 		{
 			str = NSLocalizedString(
 				self,
-				tableName: tableName+"-custom",
+				tableName: tableName+tableSuffix,
+				bundle: bundle,
+				value: defaultValue,
+				comment: comment)
+		}
+
+		// If the string was not found, then just in tableName as fallback
+		
+		if str == defaultValue
+		{
+			str = NSLocalizedString(
+				self,
+				tableName: tableName,
 				bundle: bundle,
 				value: defaultValue,
 				comment: comment)
