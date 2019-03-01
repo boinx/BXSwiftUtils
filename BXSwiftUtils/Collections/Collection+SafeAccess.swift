013184fa698ecf8daf99f71076716e9c617bdd03
Stefan Fochler <istefo@me.com>
2019-03-01 09:00:12 +0100

    Add safe range access to Collection

--- BXSwiftUtils/Collections/Collection+SafeAccess.swift
+++ BXSwiftUtils/Collections/Collection+SafeAccess.swift
@@ -14,4 +14,24 @@ extension Collection where Element: Any
     {
         return indices.contains(index) ? self[index] : nil
     }
+    
+    public subscript (safe range: Range<Index>) -> Self.SubSequence
+    {
+        let lowerBound = Swift.max(range.lowerBound, self.startIndex)
+        let upperBound = Swift.min(range.upperBound, self.endIndex)
+        
+        // If the range was completely outside of the collection's index range, return an empty sub sequence.
+        if lowerBound > upperBound
+        {
+            return self.prefix(0)
+        }
+        
+        return self[lowerBound..<upperBound]
+    }
+    
+    public subscript (safe range: ClosedRange<Index>) -> Self.SubSequence
+    {
+        // Use the implementation of safe Range access.
+        return self[safe: range.lowerBound..<self.index(after: range.upperBound)]
+    }
 }
--- BXSwiftUtilsTests/Collection/Collection+SafeAccessTests.swift
+++ BXSwiftUtilsTests/Collection/Collection+SafeAccessTests.swift
@@ -10,16 +10,46 @@ import XCTest
 import BXSwiftUtils
 
 
-class Collection_SafeAccessTests: XCTestCase {
-
-    let data = ["hello", "world"]
+class Collection_SafeAccessTests: XCTestCase
+{
+    let data = ["hello", "world", "one", "two", "three"]
+    let empty: [String] = []
     
-    func testSuccessfullAccess() {
+    func testSuccessfullAccess()
+    {
         XCTAssertEqual(data[safe: 0], "hello", "retrieves existing element")
     }
 
-    func testOutOfBoundsAccess() {
+    func testOutOfBoundsAccess()
+    {
         XCTAssertEqual(data[safe: 1337], nil, "safely returns nil")
     }
-
+    
+    func testClosedRangeAccess()
+    {
+        XCTAssertEqual(data[safe: 0...1], ["hello", "world"])
+        XCTAssertEqual(data[safe: 1...1], ["world"])
+        XCTAssertEqual(data[safe: 2...4], ["one", "two", "three"])
+        XCTAssertEqual(data[safe: 2...10], ["one", "two", "three"])
+        XCTAssertEqual(data[safe: -1...1], ["hello", "world"])
+        XCTAssertEqual(Array(data[safe: -10...10]), data)
+        
+        XCTAssertEqual(data[safe: 10...20], [])
+        XCTAssertEqual(data[safe: -20...(-10)], [])
+        XCTAssertEqual(empty[safe: -10...10], [])
+    }
+    
+    func testRangeAccess()
+    {
+        XCTAssertEqual(data[safe: 0..<2], ["hello", "world"])
+        XCTAssertEqual(data[safe: 0..<1], ["hello"])
+        XCTAssertEqual(data[safe: 2..<5], ["one", "two", "three"])
+        XCTAssertEqual(data[safe: 2..<10], ["one", "two", "three"])
+        XCTAssertEqual(data[safe: -1..<2], ["hello", "world"])
+        XCTAssertEqual(Array(data[safe: -10..<10]), data)
+        
+        XCTAssertEqual(data[safe: 10..<20], [])
+        XCTAssertEqual(data[safe: -20..<(-10)], [])
+        XCTAssertEqual(empty[safe: -10..<10], [])
+    }
 }
