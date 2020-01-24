0ea95ac94046a1f38be684abdd51d9ae6c89975d
Peter Baumgartner <peter@boinx.com>
2020-01-24 16:12:13 +0100

    Code cleanup

--- BXSwiftUtils/Enums/LocalizableIntEnum.swift
+++ BXSwiftUtils/Enums/LocalizableIntEnum.swift
@@ -13,18 +13,42 @@ import Foundation
 //----------------------------------------------------------------------------------------------------------------------
 
 
+/// Returns a localized name (and optionally an icon) for displaying an enum datatype in the user interface
+
 public protocol LocalizableIntEnum
 {
-	var intValue : Int { get }
+	/// The rawValue of this enum case. Can be used to attach to UI items as metadata.
+	
 	var rawValue : Int { get }
+	
+	/// The intValue of this enum case. Can be used to attach to UI items as metadata.
+	
+	var intValue : Int { get }
+
+	/// The localized name of this enum case for displaying in the user interface
+	
 	var localizedName : String { get }
 }
 
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
 public extension LocalizableIntEnum
 {
-	var intValue:Int { self.rawValue }
+	// intValue is just a synonym for rawValue
 	
-	var localizedName:String { "<<NOT LOCALIZED>>" }
+	var intValue:Int
+	{
+		self.rawValue
+	}
+	
+	// Default string if localization was not provided
+	
+	var localizedName:String
+	{
+		"<<NOT LOCALIZED>>"
+	}
 }
 
 
