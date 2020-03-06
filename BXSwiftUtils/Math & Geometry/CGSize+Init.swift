7e03010c59ac096d466ae1916a2cf6e2524ae127
Peter Baumgartner <peter@boinx.com>
2020-03-06 15:26:51 +0100

    Added convenience init functions to CGPoint and CGSize

---
+++ BXSwiftUtils/Math & Geometry/CGSize+Init.swift
@@ -0,0 +1,57 @@
+//**********************************************************************************************************************
+//
+//  CGSize+Init.swift
+//	Convenience init functions without the annoying argument labels
+//  Copyright ©2020 Peter Baumgartner. All rights reserved.
+//
+//**********************************************************************************************************************
+
+
+import Foundation
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+public extension CGSize
+{
+	init(_ w:CGFloat,_ h:CGFloat)
+	{
+		self.init(width:w, height:h)
+	}
+	
+	init(_ w:Double,_ h:Double)
+	{
+		self.init(width:CGFloat(w), height:CGFloat(h))
+	}
+	
+	init(_ w:Int,_ h:Int)
+	{
+		self.init(width:CGFloat(w), height:CGFloat(h))
+	}
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+public extension CGPoint
+{
+	init(_ x:CGFloat,_ y:CGFloat)
+	{
+		self.init(x:x, y:y)
+	}
+
+	init(_ x:Double,_ y:Double)
+	{
+		self.init(x:CGFloat(x), y:CGFloat(y))
+	}
+
+	init(_ x:Int,_ y:Int)
+	{
+		self.init(x:CGFloat(x), y:CGFloat(y))
+	}
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
