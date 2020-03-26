622f4c60e947fb2f6959ac1747e4c4a4bacc3668
Peter Baumgartner <peter@boinx.com>
2020-03-26 17:55:52 +0100

    Code cleanup

--- BXSwiftUtils/Threading/DispatchQueue+Throttle.swift
+++ BXSwiftUtils/Threading/DispatchQueue+Throttle.swift
@@ -20,7 +20,7 @@ extension DispatchQueue
     
     // Last time a worker block was executed for a given identifier string
     
-    private static var throttledWork: [String: WorkStatus] = [:]
+    private static var throttledWork: [String:WorkStatus] = [:]
 
     private enum WorkStatus
     {
@@ -38,24 +38,22 @@ extension DispatchQueue
 
     // MARK: -
     
-    /**
-     Debounces all blocks with the same identifier to the end of the specified interval. The block
-     will only be executed once, when no calls occur for at least the specified interval time.
-     
-     ## Example
-     ```
-     DispatchQueue.main.debounce("performExpensiveWork", interval: 0.1)
-     {
-         performExpensiveWork()
-     }
-     ```
-     
-     - parameter identifier: An identifier string that is used to coalesce block invocations together.
-     - parameter interval: The time in seconds before the block is first executed.
-     - parameter block: The closure (block) to be executed.
-    */
+	/// Debounces all blocks with the same identifier to the end of the specified interval. The block
+	/// will only be executed once, when no calls occur for at least the specified interval time.
+	///
+	/// ## Example
+	/// ```
+	/// DispatchQueue.main.debounce("performExpensiveWork", interval: 0.1)
+	/// {
+	///	   performExpensiveWork()
+	/// }
+	/// ```
+	///
+	/// - parameter identifier: An identifier string that is used to coalesce block invocations together.
+	/// - parameter interval: The time in seconds before the block is first executed.
+	/// - parameter block: The closure (block) to be executed.
     
-    public func debounce(_ identifier: String, interval: CFTimeInterval = 0.0, block: @escaping () -> Void)
+    public func debounce(_ identifier:String, interval:CFTimeInterval = 0.0, block: @escaping ()->Void)
     {
         let index = DispatchQueue.workItemIndex.increment()
         
@@ -76,42 +74,40 @@ extension DispatchQueue
         {
             let oldItem = DispatchQueue.coalescedWork[identifier]?.item
             oldItem?.cancel()
-            DispatchQueue.coalescedWork[identifier] = (index: index, item: newItem)
+            DispatchQueue.coalescedWork[identifier] = (index:index, item:newItem)
         }
         
-        self.asyncAfter(deadline: .now() + interval, execute: newItem)
+        self.asyncAfter(deadline:.now()+interval, execute:newItem)
     }
     
     
     /// Alias of `debounce(_:interval:block:)`
 	
-    public func coalesce(_ identifier: String, interval: CFTimeInterval = 0.0, block: @escaping () -> Void)
+    public func coalesce(_ identifier:String, interval:CFTimeInterval = 0.0, block: @escaping ()->Void)
     {
-        self.debounce(identifier, interval: interval, block: block)
+        self.debounce(identifier, interval:interval, block:block)
     }
 
 
 //----------------------------------------------------------------------------------------------------------------------
 
 
-    /**
-     Throttles all blocks with the same identifier to be called only once (at the beginnging) of the specified
-     interval. All remaining calls with the same identifier will be suppressed until the interval has elapsed.
-     
-     ## Example
-     ```
-     DispatchQueue.main.throttle("performExpensiveWork",interval:0.1)
-     {
-         performExpensiveWork()
-     }
-     ```
-     
-     - parameter identifier: An identifier string that is used to group block invocations together.
-     - parameter interval: The interval duration (in seconds) between block invocations.
-     - parameter block: The closure (block) to be executed.
-    */
+	/// Throttles all blocks with the same identifier to be called only once (at the beginnging) of the specified
+	/// interval. All remaining calls with the same identifier will be suppressed until the interval has elapsed.
+	///
+	/// ## Example
+	/// ```
+	/// DispatchQueue.main.throttle("performExpensiveWork",interval:0.1)
+	/// {
+	///	   performExpensiveWork()
+	/// }
+	/// ```
+	///
+	/// - parameter identifier: An identifier string that is used to group block invocations together.
+	/// - parameter interval: The interval duration (in seconds) between block invocations.
+	/// - parameter block: The closure (block) to be executed.
 
-    public func throttle(_ identifier: String, interval: CFTimeInterval = 0.0, block: @escaping () -> Void)
+    public func throttle(_ identifier:String, interval:CFTimeInterval = 0.0, block: @escaping ()->Void)
     {
         let shouldDispatch: Bool = DispatchQueue.lock.write()
         {
@@ -142,50 +138,49 @@ extension DispatchQueue
 //----------------------------------------------------------------------------------------------------------------------
 
 
-     /**
-     Throttles the execution of the specified block to match the performance capabilities of the current machine,
-     i.e. it doesn't schedule more blocks that can be handled at a given time.
-     
-     It will only schedule another block after the previous one has finished executing.
-     However, a final execution after the specified timeout is always guarranteed.
-     This makes `dynamicThrottle` the best choice for performing UI work based on events that might occur more
-     frequently than the machine is capable of handling.
-	
-     ## Example
-     
-         let semaphore = DispatchSemaphore(value: 1)
-         serialQueue.dynamicThrottle(with:semaphore)
-         {
-             performExpensiveWork()
-         }
+	/// Throttles the execution of the specified block to match the performance capabilities of the current machine,
+	/// i.e. it doesn't schedule more blocks that can be handled at a given time.
+	///
+	/// It will only schedule another block after the previous one has finished executing.
+	/// However, a final execution after the specified timeout is always guarranteed.
+	/// This makes `dynamicThrottle` the best choice for performing UI work based on events that might occur more
+	/// frequently than the machine is capable of handling.
+	///
+	/// ## Example
+	///
+	///	 let semaphore = DispatchSemaphore(value: 1)
+	///	 serialQueue.dynamicThrottle(with:semaphore)
+	///	 {
+	///		 performExpensiveWork()
+	///	 }
+	///
+	///
+	/// The following diagram depicts the scheduling and execution of tasks 1-4 using `dynamicThrottle`. Only tasks 1 and 4
+	/// are being executed, while tasks 2 and 3 were cancelled because a new task was spawned and delayed.
+	/// ```
+	///	 task1 -> |>- - - - - - - - - ->|
+	///	 task2 ->    |.(*)...X
+	///	 task3 ->            |.(*)...X
+	///	 task4 ->                    |.(*)......|>- - - - - - - - - ->|
+	///
+	///	 |       = task is scheduled
+	///	 |>      = task is scheduled & starts execution
+	///	 ->|     = task finishes execution
+	///	 .(*)... = delayed by `finalExecutionTimeout`
+	///	 X       = delayed task is cancelled by following task
+	/// ```
+	/// - parameter semaphore: A DispatchSemaphore that controls that the next block will only be schedules after the previous one has finished executing.
+	///						**Important**: Must be initialized to value 1.
+	/// - parameter delay: The delay to wait before the next block is allowed to be scheduled for execution.
+	/// - parameter finalExecutionTimeout: The timeout to wait before for the final execution.
+	/// - parameter block: The closure (block) to be executed.
 
-     
-     The following diagram depicts the scheduling and execution of tasks 1-4 using `dynamicThrottle`. Only tasks 1 and 4
-     are being executed, while tasks 2 and 3 were cancelled because a new task was spawned and delayed.
-     
-         task1 -> |>- - - - - - - - - ->|
-         task2 ->    |.(*)...X
-         task3 ->            |.(*)...X
-         task4 ->                    |.(*)......|>- - - - - - - - - ->|
-     
-         |       = task is scheduled
-         |>      = task is scheduled & starts execution
-         ->|     = task finishes execution
-         .(*)... = delayed by `finalExecutionTimeout`
-         X       = delayed task is cancelled by following task
-     
-	 - parameter semaphore: A DispatchSemaphore that controls that the next block will only be schedules after the previous one has finished executing.
-                            **Important**: Must be initialized to value 1.
-     - parameter delay: The delay to wait before the next block is allowed to be scheduled for execution.
-     - parameter finalExecutionTimeout: The timeout to wait before for the final execution.
-     - parameter block: The closure (block) to be executed.
-    */
-
-   public func dynamicThrottle(with semaphore: DispatchSemaphore, delay: CFTimeInterval = 0.01, finalExecutionTimeout: CFTimeInterval = 0.5, block: @escaping () -> Void)
+	public func dynamicThrottle(with semaphore:DispatchSemaphore, delay:CFTimeInterval = 0.01, finalExecutionTimeout:CFTimeInterval = 0.5, block: @escaping ()->Void)
     {
     	// If the semaphore is currently free, then schedule another execution of the block. After the block
     	// has finished executing, wait a little bit before signaling the semaphore again. This helps to
     	// reduce memory pressure.
+    	
 		let identifier = String(semaphore.hash)
 
 		if semaphore.wait(timeout:.now()) == .success
@@ -208,7 +203,7 @@ extension DispatchQueue
 		
 		else
 		{
-			self.coalesce(identifier, interval: finalExecutionTimeout)
+			self.coalesce(identifier, interval:finalExecutionTimeout)
 			{
 				block()
 			}
@@ -224,7 +219,7 @@ extension DispatchQueue
     /// Cancels any coalesced work that was scheduled with the coalesce(_:interval:block:) method.
     /// - parameter identifier: A unique string that identifies the scheduled work that should be canceled
 
-    public static func cancelCoalescedWork(withIdentifier identifier: String)
+    public static func cancelCoalescedWork(withIdentifier identifier:String)
     {
         DispatchQueue.lock.write()
         {
@@ -239,7 +234,7 @@ extension DispatchQueue
     /// for cleaning up pending work for a controller that is about to be deallocated. It is obviously the
     /// responsibility of the caller to give all this work identifiers with matching prefixes.
     
-    public static func cancelCoalescedWork(withPrefix prefix: String)
+    public static func cancelCoalescedWork(withPrefix prefix:String)
     {
         DispatchQueue.lock.write()
         {
