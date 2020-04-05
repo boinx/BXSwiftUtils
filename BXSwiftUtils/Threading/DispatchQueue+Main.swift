dcb61fd4b9daeba5743999a29a19632221af0d39
Peter Baumgartner <peter@boinx.com>
2020-04-05 16:50:52 +0200

    Added executeScheduledBlocks() function

--- BXSwiftUtils/Threading/DispatchQueue+Main.swift
+++ BXSwiftUtils/Threading/DispatchQueue+Main.swift
@@ -58,4 +58,18 @@ extension DispatchQueue
             self.sync(execute: block)
         }
     }
+    
+    
+    /// If the main RunLoop is not in the default mode or is block in another way, scheduled blocks may not be executed until the runloop returns
+    /// to run in the default mode. THis could be the case while inside a modal window or when using controls like sliders. In this case calling
+    /// executeScheduledBlocks() is a workaround to make sure that blocks that have already been enqueued will be executed immediately.
+    
+	public func executeScheduledBlocks()
+	{
+		if Thread.isMainThread
+		{
+			_ = RunLoop.main.limitDate(forMode:.default)
+		}
+	}
+
 }
