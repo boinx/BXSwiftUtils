ce3871d41f19d63962370bd6a5eb4ccf5a7ea552
Peter Baumgartner <peter@boinx.com>
2020-06-22 08:35:20 +0200

    Added throttled and debounds version of onChanged()

--- BXSwiftUtils/KVO & Notifications/NSObject+onChanged.swift
+++ BXSwiftUtils/KVO & Notifications/NSObject+onChanged.swift
@@ -38,7 +38,8 @@ extension NSObjectProtocol where Self:NSObject
 
 	public func onChanged<Value>(_ keypath:KeyPath<Self,Value>, options:NSKeyValueObservingOptions = .new, handler: @escaping ((Value)->Void)) -> AnyCancellable?
 	{
-		return self.publisher(for:keypath, options:options).sink(receiveValue:handler)
+		return self.publisher(for:keypath, options:options)
+			.sink(receiveValue:handler)
 	}
 
 
@@ -59,8 +60,60 @@ extension NSObjectProtocol where Self:NSObject
 
 	public func onChanged<Value>(_ keypath:KeyPath<Self,Value?>, options:NSKeyValueObservingOptions = .new, handler: @escaping ((Value?)->Void)) -> AnyCancellable?
 	{
-		return self.publisher(for:keypath, options:options).sink(receiveValue:handler)
+		return self.publisher(for:keypath, options:options)
+			.sink(receiveValue:handler)
 	}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+	@available(macOS 10.15.2, iOS 13.2, *)
+
+	public func onChanged<Value:Equatable>(_ keypath:KeyPath<Self,Value>, options:NSKeyValueObservingOptions = .new, throttle interval:DispatchQueue.SchedulerTimeType.Stride, handler: @escaping ((Value)->Void)) -> AnyCancellable?
+	{
+		return self.publisher(for:keypath, options:options)
+			.removeDuplicates { $0 == $1 }
+			.throttle(for:interval, scheduler:DispatchQueue.main, latest:true)
+			.sink(receiveValue:handler)
+	}
+
+
+	@available(macOS 10.15.2, iOS 13.2, *)
+
+	public func onChanged<Value:Equatable>(_ keypath:KeyPath<Self,Value?>, options:NSKeyValueObservingOptions = .new, throttle interval:DispatchQueue.SchedulerTimeType.Stride, handler: @escaping ((Value?)->Void)) -> AnyCancellable?
+	{
+		return self.publisher(for:keypath, options:options)
+			.removeDuplicates { $0 == $1 }
+			.throttle(for:interval, scheduler:DispatchQueue.main, latest:true)
+			.sink(receiveValue:handler)
+	}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+	@available(macOS 10.15.2, iOS 13.2, *)
+
+	public func onChanged<Value:Equatable>(_ keypath:KeyPath<Self,Value>, options:NSKeyValueObservingOptions = .new, debounce interval:DispatchQueue.SchedulerTimeType.Stride, handler: @escaping ((Value)->Void)) -> AnyCancellable?
+	{
+		return self.publisher(for:keypath, options:options)
+			.removeDuplicates { $0 == $1 }
+			.debounce(for:interval, scheduler:DispatchQueue.main)
+			.sink(receiveValue:handler)
+	}
+
+
+	@available(macOS 10.15.2, iOS 13.2, *)
+
+	public func onChanged<Value:Equatable>(_ keypath:KeyPath<Self,Value?>, options:NSKeyValueObservingOptions = .new, debounce interval:DispatchQueue.SchedulerTimeType.Stride, handler: @escaping ((Value?)->Void)) -> AnyCancellable?
+	{
+		return self.publisher(for:keypath, options:options)
+			.removeDuplicates { $0 == $1 }
+			.debounce(for:interval, scheduler:DispatchQueue.main)
+			.sink(receiveValue:handler)
+	}
+
 }
 
 
