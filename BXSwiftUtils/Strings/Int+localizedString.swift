9d37ee04e9a30cf7abcf19752e71de8143a61ff1
Peter Baumgartner <peter@boinx.com>
2020-11-04 11:28:47 +0100

    Added Int+localizedString

---
+++ BXSwiftUtils/Strings/Int+localizedString.swift
@@ -0,0 +1,58 @@
+//**********************************************************************************************************************
+//
+//  Int+localizedString.swift
+//	Localized strings for singular/plural values
+//  Copyright ©2020 Peter Baumgartner. All rights reserved.
+//
+//**********************************************************************************************************************
+
+
+public extension Int
+{
+	var localizedTimesString : String
+	{
+		let format = "%d time(s)".localized(from:"Int+localizedString", bundle:Bundle.BXSwiftUtils)
+		return String(format:format,self)
+	}
+
+	var localizedFilesString : String
+	{
+		let format = "%d file(s)".localized(from:"Int+localizedString", bundle:Bundle.BXSwiftUtils)
+		return String(format:format,self)
+	}
+
+	var localizedItemsString : String
+	{
+		let format = "%d item(s)".localized(from:"Int+localizedString", bundle:Bundle.BXSwiftUtils)
+		return String(format:format,self)
+	}
+
+	var localizedObjectsString : String
+	{
+		let format = "%d object(s)".localized(from:"Int+localizedString", bundle:Bundle.BXSwiftUtils)
+		return String(format:format,self)
+	}
+
+	var localizedImagesString : String
+	{
+		let format = "%d image(s)".localized(from:"Int+localizedString", bundle:Bundle.BXSwiftUtils)
+		return String(format:format,self)
+	}
+
+	var localizedVideosString : String
+	{
+		let format = "%d video(s)".localized(from:"Int+localizedString", bundle:Bundle.BXSwiftUtils)
+		return String(format:format,self)
+	}
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+public extension Bundle
+{
+	static let BXSwiftUtils = Bundle(identifier:"com.boinx.BXSwiftUtils")
+}
+
+//----------------------------------------------------------------------------------------------------------------------
