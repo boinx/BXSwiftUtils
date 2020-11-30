4d78629f529118587550b3d4f6272e031cc8f4b5
Peter Baumgartner <peter@boinx.com>
2020-11-30 19:43:50 +0100

    Code cleanup

--- BXSwiftUtils/Math & Geometry/CGPoint+Operators.swift
+++ BXSwiftUtils/Math & Geometry/CGPoint+Operators.swift
@@ -1,8 +1,8 @@
 //**********************************************************************************************************************
 //
-//  CGPoint+FMExtensions.swift
-//	Adds convenience methods
-//  Copyright ©2016 Peter Baumgartner. All rights reserved.
+//  CGPoint+Operators.swift
+//	Point and vector operators
+//  Copyright ©2016-2020 Peter Baumgartner. All rights reserved.
 //
 //**********************************************************************************************************************
 
@@ -14,66 +14,72 @@ import CoreGraphics
 //----------------------------------------------------------------------------------------------------------------------
 
 
+public typealias CGVector = CGPoint
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
 /// This extension adds some vector math operators to CGPoint/NSPoint
 
 extension CGPoint
 {
-	public static func + (l: CGPoint, r: CGPoint) -> CGPoint
+	public static func + (l:CGPoint, r:CGPoint) -> CGPoint
 	{
 		return CGPoint(x:l.x+r.x,y:l.y+r.y)
 	}
 	
-	public static func - (l: CGPoint, r: CGPoint) -> CGPoint
+	public static func - (l:CGPoint, r:CGPoint) -> CGPoint
 	{
 		return CGPoint(x:l.x-r.x,y:l.y-r.y)
 	}
 
-	public static func * (l: CGPoint, r: CGPoint) -> CGFloat
+	public static func * (l:CGPoint, r:CGPoint) -> CGFloat
 	{
 		return l.x*r.x + l.y*r.y
 	}
 
-	public static func * (l: CGPoint, r: CGFloat) -> CGPoint
+	public static func * (l:CGPoint, r:CGFloat) -> CGPoint
 	{
 		return CGPoint(x:l.x*r,y:l.y*r)
 	}
 
-	public static func * (l: CGFloat, r: CGPoint) -> CGPoint
+	public static func * (l:CGFloat, r:CGPoint) -> CGPoint
 	{
 		return CGPoint(x:l*r.x,y:l*r.y)
 	}
 
-	public static func * (l: CGPoint, r: Double) -> CGPoint
+	public static func * (l:CGPoint, r:Double) -> CGPoint
 	{
 		return CGPoint(x:l.x*CGFloat(r),y:l.y*CGFloat(r))
 	}
 
-	public static func * (l: Double, r: CGPoint) -> CGPoint
+	public static func * (l:Double, r:CGPoint) -> CGPoint
 	{
 		return CGPoint(x:CGFloat(l)*r.x,y:CGFloat(l)*r.y)
 	}
 
-	public static func / (l: CGPoint, r: CGFloat) -> CGPoint
+	public static func / (l:CGPoint, r:CGFloat) -> CGPoint
 	{
 		return CGPoint(x:l.x/r,y:l.y/r)
 	}
 
-	public static func / (l: CGPoint, r: Double) -> CGPoint
+	public static func / (l:CGPoint, r:Double) -> CGPoint
 	{
 		return CGPoint(x:l.x/CGFloat(r),y:l.y/CGFloat(r))
 	}
 
-	public static func += (l:inout CGPoint, r: CGPoint)
+	public static func += (l:inout CGPoint, r:CGPoint)
 	{
 		l = CGPoint(x:l.x+r.x,y:l.y+r.y)
 	}
 
-	public static func *= (l:inout CGPoint, r: CGFloat)
+	public static func *= (l:inout CGPoint, r:CGFloat)
 	{
 		l = CGPoint(x:l.x*r,y:l.y*r)
 	}
 
-	public static func *= (l:inout CGPoint, r: Double)
+	public static func *= (l:inout CGPoint, r:Double)
 	{
 		l = CGPoint(x:l.x*CGFloat(r),y:l.y*CGFloat(r))
 	}
@@ -87,21 +93,21 @@ extension CGPoint
 	
 	/// Normalized a vector to length 1.0, keeping is direction intact
 	
-	public var normalized: CGPoint
+	public var normalized:CGPoint
 	{
 		return self / self.length
 	}
 	
 	/// Returns the mid point between to points
 	
-	public static func midPoint(between p1:CGPoint,and p2:CGPoint) -> CGPoint
+	public static func midPoint(between p1:CGPoint, and p2:CGPoint) -> CGPoint
 	{
-		return self.interpolate( between:p1, and:p2, factor:0.5 )
+		return self.interpolate( between:p1, and:p2, factor:0.5)
 	}
 
 	/// Returns the mid point between to points
 	
-	public static func interpolate(between p1:CGPoint,and p2:CGPoint,factor:CGFloat) -> CGPoint
+	public static func interpolate(between p1:CGPoint, and p2:CGPoint, factor:CGFloat) -> CGPoint
 	{
 		return CGPoint(
 			x: (1.0-factor) * p1.x + factor * p2.x,
