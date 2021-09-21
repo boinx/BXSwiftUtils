447eb1437b6b00b04cd435a5fee7379566b5419e
Peter Baumgartner <peter@boinx.com>
2021-09-21 15:46:15 +0200

    Added function to demangle swift function names

---
+++ BXSwiftUtils/Strings/String+demangle.swift
@@ -0,0 +1,53 @@
+
+
+// Taken from https://stackoverflow.com/questions/24321773/how-can-i-demangle-a-swift-class-name-dynamically/25990097
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+typealias Swift_Demangle = @convention(c) (_ mangledName: UnsafePointer<UInt8>?,
+                                           _ mangledNameLength: Int,
+                                           _ outputBuffer: UnsafeMutablePointer<UInt8>?,
+                                           _ outputBufferSize: UnsafeMutablePointer<Int>?,
+                                           _ flags: UInt32) -> UnsafeMutablePointer<Int8>?
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+/// Returns the human readable form of a mangled swift function name
+
+public func swift_demangle(_ mangled:String) -> String?
+{
+    let RTLD_DEFAULT = dlopen(nil, RTLD_NOW)
+    if let sym = dlsym(RTLD_DEFAULT, "swift_demangle")
+    {
+        let f = unsafeBitCast(sym, to: Swift_Demangle.self)
+        if let cString = f(mangled, mangled.count, nil, nil, 0)
+        {
+            defer { cString.deallocate() }
+            return String(cString: cString)
+        }
+    }
+    return nil
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+public extension String
+{
+	/// Returns the human readable form of a mangled swift function name
+	
+	func demangled() -> String
+	{
+		return swift_demangle(self) ?? self
+	}
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
