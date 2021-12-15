f928f4ab07c7d7cff944f0e80f66272a76563e7e
Peter Baumgartner <peter@boinx.com>
2021-12-15 17:48:51 +0100

    Added ProcessInfo+Version extension

---
+++ BXSwiftUtils/Process/ProcessInfo+Version.swift
@@ -0,0 +1,41 @@
+//**********************************************************************************************************************
+//
+//  ProcessInfo+Version.swift
+//	Adds new methods to Bundle
+//  Copyright ©2021 Peter Baumgartner. All rights reserved.
+//
+//**********************************************************************************************************************
+
+
+import Foundation
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+public extension ProcessInfo
+{
+	/// Returns the major version of the OS we are running on
+	
+	static var osMajorVersion:Int
+	{
+		ProcessInfo.processInfo.operatingSystemVersion.majorVersion
+	}
+
+	#if os(macOS)
+	
+	static var isRunningOnBigSurOrNewer:Bool
+	{
+		Self.osMajorVersion >= 11
+	}
+
+	static var isRunningOnMontereyOrNewer:Bool
+	{
+		Self.osMajorVersion >= 12
+	}
+	
+	#endif
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
