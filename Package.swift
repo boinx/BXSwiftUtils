b523d0a18154961d0d8f711a05204b89b1ea48f0
Peter Baumgartner <peter@boinx.com>
2022-01-27 10:34:30 +0100

    Added SwiftPM package manifest file

---
+++ Package.swift
@@ -0,0 +1,45 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+
+    name: "BXSwiftUtils",
+    
+    platforms:
+    [
+		.macOS(.v10_11),
+		.iOS(.v11)
+    ],
+    
+    products:
+    [
+        // Products define the executables and libraries a package produces, and make them visible to other packages
+        
+        .library(
+            name: "BXSwiftUtils",
+            targets: ["BXSwiftUtils"]),
+    ],
+    
+    dependencies:
+    [
+        // Dependencies declare other packages that this package depends on
+        
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    
+    targets:
+    [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+
+        .target(
+            name: "BXSwiftUtils",
+            dependencies: []),
+            
+//        .testTarget(
+//            name: "BXMediaBrowserTests",
+//            dependencies: ["BXMediaBrowser"]),
+    ]
+)
