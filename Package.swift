957fa45bfe221862b41e97cd82deef367cf3247f
Peter Baumgartner <peter@boinx.com>
2022-01-27 15:35:16 +0100

    Added defaultLocalization to package manifest

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ import PackageDescription
 let package = Package(
 
     name: "BXSwiftUtils",
+    defaultLocalization: "en",
     
     platforms:
     [
@@ -13,33 +14,26 @@ let package = Package(
 		.iOS(.v11)
     ],
     
+	// Products define the executables and libraries a package produces, and make them visible to other packages
+        
     products:
     [
-        // Products define the executables and libraries a package produces, and make them visible to other packages
-        
-        .library(
-            name: "BXSwiftUtils",
-            targets: ["BXSwiftUtils"]),
+        .library(name:"BXSwiftUtils", targets:["BXSwiftUtils"]),
     ],
     
+	// Dependencies declare other packages that this package depends on
+	
     dependencies:
     [
-        // Dependencies declare other packages that this package depends on
-        
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     
+	// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+	// Targets can depend on other targets in this package, and on products in packages this package depends on.
+
     targets:
     [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-
-        .target(
-            name: "BXSwiftUtils",
-            dependencies: []),
-            
-//        .testTarget(
-//            name: "BXMediaBrowserTests",
-//            dependencies: ["BXMediaBrowser"]),
+        .target(name:"BXSwiftUtils", dependencies:[]),
+//		.testTarget( name:"BXMediaBrowserTests", dependencies:["BXMediaBrowser"]),
     ]
 )
