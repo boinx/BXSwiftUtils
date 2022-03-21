e02c6a621285b6c2f0e112d17f692fb438eb0915
Peter Baumgartner <peter@boinx.com>
2022-03-21 14:12:31 +0100

    Commented out unused functions

--- Sources/BXSwiftUtils/Data/Data+TypeCasting.swift
+++ Sources/BXSwiftUtils/Data/Data+TypeCasting.swift
@@ -28,10 +28,10 @@ public extension Data
 	/// Converts a Data object to a value WITHOUT copying the underlying memory
 	/// - parameter type: The type of the value
 
-    func `as`<T>(type: T.Type) -> T
-    {
-        return self.withUnsafeBytes { $0.pointee }
-    }
+//    func `as`<T>(type: T.Type) -> T
+//    {
+//        return self.withUnsafeBytes { $0.pointee }
+//    }
 	
 	
 	/// Wraps an Array in a Data object WITHOUT copying the underlying memory
@@ -48,15 +48,51 @@ public extension Data
 	/// Converts a Data object to an Array WITHOUT copying the underlying memory
 	/// - parameter type: The type of the array elements
 
-    func asArray<T>(ofType: T.Type) -> [T]
-    {
-    	let count = self.count / MemoryLayout<T>.stride
-		
-        return self.withUnsafeBytes
-        {
-            [T](UnsafeBufferPointer(start:$0, count:count))
-        }
-    }
+//    func asArray<T>(ofType: T.Type) -> [T]
+//    {
+//    	let count = self.count / MemoryLayout<T>.stride
+//
+//        return self.withUnsafeBytes
+//        {
+//            [T](UnsafeBufferPointer(start:$0, count:count))
+//        }
+//    }
+
+
+	/// Presents a Data as a typed array. Please note that this representation is only availalble inside the
+	/// provided closure.
+	
+//	func withArray<T>(ofType type:T.Type, _ closure:([T]) throws -> Void) rethrows
+//	{
+//		try self.withUnsafeBytes
+//		{
+//			guard let ptr = $0.baseAddress?.assumingMemoryBound(to:type) else { return }
+//			
+//			let n1 = self.count
+//			let n2 = n1 / MemoryLayout<T>.stride
+//			let buffer = UnsafeBufferPointer(start:ptr, count:n2)
+//			let array = Array(buffer)
+//			
+//			try closure(array)
+//		}
+//	}
+//	
+//	func withMutableArray<T>(ofType type:T.Type, _ closure:(inout [T]) throws -> Void) rethrows
+//	{
+//		try self.withUnsafeBytes
+//		{
+//			guard let ptr = $0.baseAddress?.assumingMemoryBound(to:type) else { return }
+//			
+//			let n1 = self.count
+//			let n2 = n1 / MemoryLayout<T>.stride
+//			let buffer = UnsafeBufferPointer(start:ptr, count:n2)
+//			var array = Array(buffer)
+//			
+//			try closure(&array)
+//		}
+//	}
+	
+
 
 }
 
