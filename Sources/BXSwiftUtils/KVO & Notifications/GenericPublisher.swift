2cc892ddc70573fc0fd74c3294eb0a9c616863bb
Peter Baumgartner <peter@boinx.com>
2022-06-01 08:22:10 +0200

    Added GenericPublisher convenience type

---
+++ Sources/BXSwiftUtils/KVO & Notifications/GenericPublisher.swift
@@ -0,0 +1,48 @@
+//**********************************************************************************************************************
+//
+//  GenericPublisher.swift
+//	Adds convenience APIs for Combine publishers
+//  Copyright ©2022 Peter Baumgartner. All rights reserved.
+//
+//**********************************************************************************************************************
+
+
+#if canImport(Combine)
+
+import Combine
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+/// GenericPublisher can be used at the call site to trigger work based on a event stream
+
+@available(macOS 10.15, iOS 13, *)
+public typealias GenericPublisher = PassthroughSubject<Void,Never>
+
+@available(macOS 10.15, iOS 13, *)
+public extension GenericPublisher
+{
+	// These function with different sames (but identical functionality) are provide better readbility at the call site
+	
+	func send()
+	{
+		self.send(())
+	}
+	
+	func request()
+	{
+		self.send()
+	}
+	
+	func setNeedsUpdate()
+	{
+		self.send()
+	}
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+#endif
