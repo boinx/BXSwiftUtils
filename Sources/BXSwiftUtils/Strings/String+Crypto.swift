25710f66da9e0177248f4df58d9408cdf15f6922
Peter Baumgartner <peter@boinx.com>
2025-01-13 15:43:02 +0100

    Migrated from deprecated CC_MD5(…) to  CC_SHA256(…)

--- Sources/BXSwiftUtils/Strings/String+Crypto.swift
+++ Sources/BXSwiftUtils/Strings/String+Crypto.swift
@@ -2,7 +2,7 @@
 //
 //  String+Crypto.swift
 //	Functions to scramble a String
-//  Copyright ©2021 Peter Baumgartner. All rights reserved.
+//  Copyright ©2021-2025 Peter Baumgartner. All rights reserved.
 //
 //**********************************************************************************************************************
 
@@ -16,36 +16,67 @@ import CommonCrypto
 
 public extension String
 {
-	var md5 : String
-	{
-        let length = Int(CC_MD5_DIGEST_LENGTH)
+	/// Returns a SHA256 encrypted version of this string
+	
+    var sha256:String
+    {
+        let length = Int(CC_SHA256_DIGEST_LENGTH)
         let messageData = self.data(using:.utf8)!
         var digestData = Data(count:length)
 
-        _ = digestData.withUnsafeMutableBytes
+        digestData.withUnsafeMutableBytes
         {
-			digestBytes -> UInt8 in
+			digestBytes in
 			
             messageData.withUnsafeBytes
             {
-				messageBytes -> UInt8 in
+				messageBytes in
 				
                 if let messageBytesBaseAddress = messageBytes.baseAddress,
-                   let digestBytesBlindMemory = digestBytes.bindMemory(to: UInt8.self).baseAddress
-                {
-                    let messageLength = CC_LONG(messageData.count)
-                    CC_MD5(messageBytesBaseAddress, messageLength, digestBytesBlindMemory)
+                   let digestBytesBaseAddress = digestBytes.bindMemory(to: UInt8.self).baseAddress
+				{
+                	let messageLength = CC_LONG(messageData.count)
+                    CC_SHA256(messageBytesBaseAddress, messageLength, digestBytesBaseAddress)
                 }
-                
-                return 0
             }
         }
-        
-        return digestData.map
-        {
-			String(format: "%02hhx", $0) }.joined()
-		}
 
+        return digestData.map { String(format:"%02hhx",$0) }.joined()
+    }
+
+
+	/// Returns a MD5 encrypted version of this string.
+	///
+	/// Please note that MD% is deprecated and should not be used anymore, but I didn't delete the code yet, just in case we need
+	/// to use it for some reason in the future.
+
+//	var md5:String
+//	{
+//        let length = Int(CC_MD5_DIGEST_LENGTH)
+//        let messageData = self.data(using:.utf8)!
+//        var digestData = Data(count:length)
+//
+//        _ = digestData.withUnsafeMutableBytes
+//        {
+//			digestBytes -> UInt8 in
+//			
+//            messageData.withUnsafeBytes
+//            {
+//				messageBytes -> UInt8 in
+//				
+//                if let messageBytesBaseAddress = messageBytes.baseAddress,
+//                   let digestBytesBlindMemory = digestBytes.bindMemory(to: UInt8.self).baseAddress
+//                {
+//                    let messageLength = CC_LONG(messageData.count)
+//                    CC_MD5(messageBytesBaseAddress, messageLength, digestBytesBlindMemory)
+//                }
+//                
+//                return 0
+//            }
+//        }
+//        
+//        return digestData.map { String(format:"%02hhx",$0) }.joined()
+//	}
 }
 
 
